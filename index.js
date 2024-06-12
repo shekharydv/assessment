03a1fa03a35e5d2bf56e3d9567bea3306ecca69f
Shekhar Yadav <32586454+shekharydv@users.noreply.github.com>
2024-06-12 20:36:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-function compareTriplets( [17 28 30] a, List[99 16 8] b) {
-    List<Integer> result = new ArrayList<Integer>(2);
-    for (int i = 0; i < 2; i++) {
-        result.add(0);
-    }
-    for(int i=0;i<3;i++) {
-        if(a.get(i)>b.get(i)) {
-            result.set(0, result.get(0)+1);
+function compareTriplets(a, b) {
+  
+    let alicePoints = 0;
+    let bobPoints = 0;
+    
+ 
+    for (let i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            alicePoints++;
+        } else if (a[i] < b[i]) {
+            bobPoints++;
         }
-        else if(a.get(i)<b.get(i)) {
-            result.set(1, result.get(1) + 1);
-        }
     }
-    return result;
+    
+    return [alicePoints, bobPoints];
 }
+
+let a = [17, 28, 30];
+let b = [99, 16, 8];
+console.log(compareTriplets(a, b));  
